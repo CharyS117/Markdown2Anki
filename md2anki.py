fb901375b8c16c4010b4703f0bbb30c33cb6418d
chary <42903955+CharyS117@users.noreply.github.com>
2021-05-08 14:15:58 +0800

    Update md2anki.py

--- md2anki.py
+++ md2anki.py
@@ -2,88 +2,100 @@ import pandas as pd
 import os
 
 separator = '\n\n'
+outPath = './'
+filePath = './'
 
-def JudgeMath(dl,i):
+def JudgeMath(dl, i):
     if len(dl) > 1:
-        if i != 0 and i != len(dl)-1:
-            if dl[i+1] == '$':
+        if i != 0 and i != len(dl) - 1:
+            if dl[i + 1] == '$':
                 return 1
-            if dl[i-1] != '$':
+            if dl[i - 1] != '$':
                 return 0
         if i == 0:
-            if dl[i+1] != '$':
+            if dl[i + 1] != '$':
                 return 0
             else:
                 return 1
         if i == len(dl):
-            if dl[i-1] != '$':
+            if dl[i - 1] != '$':
                 return 0
 
-def JudgeBold(dl,i):
+
+def JudgeBold(dl, i):
     if len(dl) > 1:
-        if i != 0 and i != len(dl)-1:
-            if dl[i+1] == '*':
+        if i != 0 and i != len(dl) - 1:
+            if dl[i + 1] == '*':
                 return 1
-            if dl[i-1] != '*':
+            if dl[i - 1] != '*':
                 return 0
         if i == 0:
-            if dl[i+1] != '*':
+            if dl[i + 1] != '*':
                 return 0
             else:
                 return 1
         if i == len(dl):
-            if dl[i-1] != '*':
+            if dl[i - 1] != '*':
                 return 0
 
-filenames = os.listdir()
+def JudgeMtime(filePath,outPath):
+    if os.path.exists(outPath):
+        if os.stat(outPath).st_mtime >= os.stat(filePath).st_mtime:
+            return 0
+    return 1
+
+filenames = os.listdir(filePath)
 outnames = []
 i = 0
 n = len(filenames)
 while i < n:
     if '.md' in filenames[i]:
-        outnames.append(filenames[i].split('.md')[0])
+        outnames.append(os.path.join(outPath, filenames[i][:-3]+'.csv'))
+        filenames[i] = os.path.join(filePath,filenames[i])
         i = i + 1
     else:
         filenames.pop(i)
         n = n - 1
-for i in range(len(outnames)):
-    outnames[i] = outnames[i] + '.csv'
-
 for p in range(len(filenames)):
-    with open(filenames[p],'r') as og:
+    if JudgeMtime(filenames[p],outnames[p]) == 0:
+        continue
+    with open(filenames[p], 'r') as og:
         og_data = og.read()
         og_list = og_data.split(separator)
         if len(og_list) % 2 == 1:
             og_list.append('')
-        df = pd.DataFrame(og_list[1::2],index=og_list[::2])
-        df.to_csv(outnames[p],encoding='utf_8_sig',columns=None,header=None)
-    with open(outnames[p],'r') as csv:
+        df = pd.DataFrame(og_list[1::2], index=og_list[::2])
+        df.to_csv(outnames[p],
+                  encoding='utf_8_sig',
+                  columns=None,
+                  header=None)
+    with open(outnames[p], 'r') as csv:
         math_k = 0
         bold_k = 0
         quotes_k = 0
         data = csv.read()
         dl = list(data)
-        for i in range(len(dl)-1):
+        for i in range(len(dl) - 1):
             if dl[i] == '$':
-                math = JudgeMath(dl,i)
+                math = JudgeMath(dl, i)
                 if math == 0:
-                        if math_k == 0:
-                            dl[i] = '\\('
-                            math_k = 1
-                        else:
-                            dl[i] = '\\)'
-                            math_k = 0
+                    if math_k == 0:
+                        dl[i] = '\\('
+                        math_k = 1
+                    else:
+                        dl[i] = '\\)'
+                        math_k = 0
                 if math == 1:
                     if math_k == 0:
                         dl[i] = '\\['
-                        dl[i+1] = ''
+                        dl[i + 1] = ''
                         math_k = 1
                     else:
                         dl[i] = '\\]'
-                        dl[i+1] = ''
+                        dl[i + 1] = ''
                         math_k = 0
             if dl[i] == '*' and math_k == 0:
-                bold = JudgeBold(dl,i)
+                bold = JudgeBold(dl, i)
                 if bold == 0:
                     if bold_k == 0:
                         dl[i] = '<i>'
@@ -94,11 +106,11 @@ for p in range(len(filenames)):
                 if bold == 1:
                     if bold_k == 0:
                         dl[i] = '<b>'
-                        dl[i+1] = ''
+                        dl[i + 1] = ''
                         bold_k = 1
                     else:
                         dl[i] = '</b>'
-                        dl[i+1] = ''
+                        dl[i + 1] = ''
                         bold_k = 0
             if dl[i] == '<' and math_k == 1:
                 dl[i] = '&lt;'
@@ -110,7 +122,7 @@ for p in range(len(filenames)):
                 else:
                     quotes_k = 0
             if quotes_k == 1 and dl[i] == '\n':
-               dl[i] = '<br>'
-        with open(outnames[p],'w') as csv:
+                dl[i] = '<br>'
+        with open(outnames[p], 'w') as csv:
             for i in dl:
                 csv.write(i)
